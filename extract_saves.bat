1c7d9039b3daa720fb0f972403665d7acd3e5d15
Vittorio Mignini <m1gnus@protonmail.com>
2020-10-09 09:32:18 +0000

    Update extract_saves.bat

--- extract_saves.bat
+++ extract_saves.bat
@@ -2,5 +2,5 @@ IF "%~1"=="" (
 	xcopy /s %USERPROFILE%\AppData\LocalLow\noio %USERPROFILE%\Desktop\
 ) ELSE (
 	MKDIR %USERPROFILE%\AppData\LocalLow\noio\KingdomTwoCrowns
-	xcopy /s /Y %~1 %PATHD% %USERPROFILE%\AppData\LocalLow\noio\KingdomTwoCrowns
+	xcopy /s /Y %~1 %USERPROFILE%\AppData\LocalLow\noio\KingdomTwoCrowns
 )
